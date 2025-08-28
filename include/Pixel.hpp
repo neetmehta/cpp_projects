df5574198f6e54672b33725c6c46a4ba0ae173a5
neetmehta <neetmehta19@gmail.com>
2025-08-28 16:05:05 +0530

    added git ignore

--- include/Pixel.hpp
+++ include/Pixel.hpp
@@ -1,24 +1,24 @@
 struct Pixel
 {
 private:
-    unsigned int i;     // Position
-    unsigned int j;     // Position
+    int i;     // Position
+    int j;     // Position
     unsigned int I = 0; // Intensity
     unsigned int score = 0; // Score for corner strength
 
 public:
-    Pixel(unsigned int _i, unsigned int _j, unsigned int _I, unsigned int _score) : i(_i), j(_j), I(_I), score(_score) {}
+    Pixel(int _i, int _j, unsigned int _I, unsigned int _score) : i(_i), j(_j), I(_I), score(_score) {}
     Pixel() = default;
 
     // Getters
-    unsigned int getI() const { return i; }
-    unsigned int getJ() const { return j; }
+    int getI() const { return i; }
+    int getJ() const { return j; }
     unsigned int getIntensity() const { return I; }
     unsigned int getScore() const { return score; }
 
     // Setters
-    void setI(unsigned int value) { i = value; }
-    void setJ(unsigned int value) { j = value; }
+    void setI(int value) { i = value; }
+    void setJ(int value) { j = value; }
     void setIntensity(unsigned int value) { I = value; }
     void setScore(unsigned int value) { score = value; }
 
